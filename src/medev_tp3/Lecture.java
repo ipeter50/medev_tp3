d27b1ffbae9883eea1ebc27185e1df985bd15e60
theJpay <jules.paris@wanadoo.fr>
2017-11-23 11:09:33 +0100

    Please enter the commit messagefor your changes. Lines starting

--- src/medev_tp3/Lecture.java
+++ src/medev_tp3/Lecture.java
@@ -6,6 +6,8 @@
 package medev_tp3;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -14,15 +16,36 @@ import java.io.*;
 public class Lecture {
 
     public static Image LecturePGM(String source) {
-        Image img = new Image();
+        Image img = new Image(0,0);
         try {
             String line;
             BufferedReader fichier = new BufferedReader(new FileReader(source));
             line = fichier.readLine();
+            if (line.contains("P2")) {
+                fichier.readLine();
+                line = fichier.readLine();
+                StringTokenizer t = new StringTokenizer(line, " ");
+                img.setHauteur(Integer.parseInt(t.nextToken()));
+                img.setLargeur(Integer.parseInt(t.nextToken()));
+                fichier.readLine();
+                line = fichier.readLine();
+                int i = 0;
+                while (line != null) {
+                    img.getPixels().add(new ArrayList<Integer>());
+                    t = new StringTokenizer(line, "\t");
+                    System.out.println(i);
+                    while (t.hasMoreTokens()) {
+                        String pixel = t.nextToken();
+                        img.getPixels().get(i).add(Integer.parseInt(pixel));
+                    }
+                    i++;
+                    line = fichier.readLine();
+                }
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        return img;
     }
 }

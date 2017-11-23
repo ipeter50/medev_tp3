dc638deb6cefa4d9270ca290883213e5c39ecf67
ipeter50 <pierre.le-jeune@live.fr>
2017-11-23 10:24:03 +0100

    Seuil et histogramme

--- src/medev_tp3/Image.java
+++ src/medev_tp3/Image.java
@@ -46,6 +46,60 @@ public class Image {
         this.pixels=new ArrayList<ArrayList<Integer>>(); 
     }
     
+    public Image histogramme(){
+        ArrayList<Integer> niveaux = new ArrayList<Integer>();
+        for (int i=0; i<256;i++){
+            niveaux.add(0);
+        }
+        
+        for (int i=0;i<this.hauteur;i++){
+            for (int j=0;j<this.largeur;j++){
+                niveaux.set(this.pixels.get(i).get(j),niveaux.get(this.pixels.get(i).get(j))+1);
+            }
+        }
+        Image hist=new Image(this.max(niveaux),256);
+        
+        for(int i=0;i<hist.hauteur;i++){
+            for(int j=0;j<hist.largeur;j++){
+                hist.pixels.get(j).set(i,255);
+            }
+            
+        }
+        for(int i=0;i<niveaux.size();i++){
+            for(int j=0;j<niveaux.get(i);j++){
+                hist.pixels.get(j).set(i,0);
+            }
+            
+        }
+        return hist;
+    }
+    
+    public int max(ArrayList<Integer> niveaux){
+        int max=0;
+        for (int i=0;i<niveaux.size();i++){
+            if(niveaux.get(i)>max){
+                max=niveaux.get(i);
+            }
+        }
+        return max;
+    }
+    
+    public void seuillage(int seuil){
+        for(int i=0;i<this.hauteur;i++){
+            for(int j=0;j<this.largeur;j++){
+                if(this.pixels.get(i).get(j)>=seuil){
+                    this.pixels.get(j).set(i,255);
+                }
+                else{
+                    this.pixels.get(j).set(i,0);
+                }
+                
+            }
+            
+        }
+        
+    }
+    
     
     
 }

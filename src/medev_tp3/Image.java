fcf48011dbfab6b40e0c6d8df9e834082c2d1702
ipeter50 <pierre.le-jeune@live.fr>
2017-11-23 09:39:21 +0100

    Ajout de Image

---
+++ src/medev_tp3/Image.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medev_tp3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Pierre
+ */
+public class Image {
+    private int hauteur;
+    private int largeur;
+    private ArrayList<ArrayList<Integer>> pixels;
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
+
+    public ArrayList<ArrayList<Integer>> getPixels() {
+        return pixels;
+    }
+
+    public void setPixels(ArrayList<ArrayList<Integer>> pixels) {
+        this.pixels = pixels;
+    }
+
+    public Image(int hauteur, int largeur) {
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+        this.pixels=new ArrayList<ArrayList<Integer>>(); 
+    }
+    
+    
+    
+}
